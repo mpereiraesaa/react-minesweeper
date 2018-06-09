be22f0b987eb9bee12ed52f251a2f5ec58bce820
manuel <manuelpereiralds@gmail.com>
2018-06-08 21:03:44 -0400

    Started UI work, the main screen for the game is ready

--- src/App.js
+++ src/App.js
@@ -8,11 +8,109 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Minesweeper</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="game-screen">
+            <div className='game-table'>
+                <div className='game-table__board'>
+                    <div className='game-table__row'>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                    </div>
+                    <div className='game-table__row'>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                    </div>
+                    <div className='game-table__row'>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                    </div>
+                    <div className='game-table__row'>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                    </div>
+                    <div className='game-table__row'>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                    </div>
+                    <div className='game-table__row'>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                    </div>
+                    <div className='game-table__row'>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                        <div className='game-table__square'></div>
+                    </div>
+                </div>
+            </div>
+            <div className='sidebar'>
+                <div className='sidebar__btns-container'>
+                    <button className='btn btn-primary'>New game</button>
+                    <button className='btn btn-primary'>Resume game</button>
+                    <div className='form-group'>
+                        <label>Columns</label>
+                        <input type='number' className='form-control' />
+                    </div>
+                    <div className='form-group'>
+                        <label>Rows</label>
+                        <input type='number' className='form-control' />
+                    </div>
+                    <div className='form-group'>
+                        <label>Mines</label>
+                        <input type='number' className='form-control' />
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
